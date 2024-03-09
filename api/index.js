81db54a3bf39b593967bef9e5586c9a05d751f21
omta <omtadayma2001@gmail.com>
2024-03-09 14:41:49 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -6,12 +6,15 @@ import authRoutes from  './routes/authRoute.js'
 import postRoutes from  './routes/postRoute.js'
 import commentRoutes from './routes/commentRoute.js'
 import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
 
 
 mongoose.connect(process.env.MONGODB).then(() => {
     console.log("Connected to MongoDB");
     }).catch((err) => {console.error(err);});
+
+    const __dirname = path.resolve();
 
 const app = express();
 app.use(express.json());
@@ -25,6 +28,13 @@ app.use('/api/user',userRoutes);
 app.use("/api/auth",authRoutes);
 app.use("/api/post",postRoutes);
 app.use('/api/comment', commentRoutes);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
+
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
